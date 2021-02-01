073a43e219272f908ecc380df75aef86ee2de28a
Shang-QY <55442231+Shang-QY@users.noreply.github.com>
2021-02-01 11:10:27 +0800

    多点温度采集与发送

--- DS18B20.py
+++ DS18B20.py
@@ -5,7 +5,6 @@ from datetime import datetime
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
-# You can generate a Token from the "Tokens Tab" in the UI
 token = "QIypCZDwHiEpUaRe10h1XaOYQbVcvPENWH_84ejDF-pUYSblnEv8RGlrkCaiQfYwEU75rHcN48EIhocGSSoHgg=="
 org = "sqy"
 bucket = "test"
